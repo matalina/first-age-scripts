4f9607953f26e51a524e8b16a230cf1c0510183b
Alicia Wilkerson <matalina@gmail.com>
2020-09-23 13:04:39 +0000

    updated the story so far

--- resources/views/story.blade.php
+++ resources/views/story.blade.php
@@ -1,30 +1,42 @@
-<h1>
-    {{ $title }}
-    <small>{{ $dir }}</small>
-</h1>
+<style>
+    #app {
+        font-family: sans-serif;
+        font-size: 12pt;
+    }
+</style>
 
-<table>
-    <thead>
-        <tr>
-            <th>Thread</th>
-            <th>Participants</th>
-            <th>Date</th>
-        </tr>
-    </thead>
-    <tbody>
-    @foreach($threads as $t)
-        <tr>
-            <td>
-                <a href="http://142.93.73.63/forums/thread-{{ $t->tid}}-post-{{ $t->firstpost }}.html">{{ $t->subject }}</a> <br/>
-                <small>on {{ $t->forum->name }}</small>
-            </td>
-            <td>
-                @foreach(($t->participants->unique('uid'))->all() as $p)
-                 {{ $p->username }} 
-                @endforeach
-            </td>
-            <td>{{ (Carbon\Carbon::createFromTimestamp($t->dateline))->format('F j, Y h:i:s A') }}</td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+<div id="app">
+    <h1>
+        {{ $title }}
+        <small> {{ $dir }} </small>
+    </h1>
+    <p>
+        <a href="{{ route('story.full', ['dir' => 'forward']) }}">Forward</a>
+        <a href="{{ route('story.full', ['dir' => 'reverse']) }}">Reverse</a>
+    </p>
+    <table>
+        <thead>
+            <tr>
+                <th width="25%">Thread</th>
+                <th width="50%">Participants</th>
+                <th width="25%">Date</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach($threads as $t)
+            <tr>
+                <td width="25%">
+                    <a href="http://142.93.73.63/forums/thread-{{ $t->tid}}-post-{{ $t->firstpost }}.html">{{ $t->subject }}</a> <br/>
+                    <small>on {{ $t->forum->name }}</small>
+                </td>
+                <td width="50%">
+                    @foreach(($t->participants->unique('uid'))->all() as $p)
+                     {{ $p->username }} 
+                    @endforeach
+                </td>
+                <td width="25%">{{ (Carbon\Carbon::createFromTimestamp($t->dateline))->format('F j, Y h:i:s A') }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
